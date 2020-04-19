1b07395ce845f20ed8425b6ef7bdbaa12513423e
Sandeep Pushp <33807748+Sandeeppushp@users.noreply.github.com>
2020-04-19 23:45:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,39 @@ tableStructure=conn.execute("SELECT sql FROM sqlite_master WHERE name = 'company
 print(tableStructure[0][0])
 
 
+
+'------------------------------------'
+name=['rahul','ankit','amit','saurabh','sahil']
+age=[21,23,22,24,23]
+department=['IT','Frontend','Backend','Marketing','UX']
+
+
+for i in range(0,len(name)):
+    conn.execute('Insert into company_table(name,age,department) values(?,?,?)',(name[i],age[i],department[i]))
+    conn.commit()
+
+
+
+
+'------------------------------------'
+from xlrd import open_workbook
+book = open_workbook("exceldata.xlsx")
+sheet = book.sheet_by_index(0)
+
+row=0
+for i in range(1,sheet.nrows):
+    name=sheet.cell(row, 0).value
+    age=sheet.cell(row, 1).value
+    department=sheet.cell(row, 2).value
+    conn.execute('Insert into company_table(name,age,department) values(?,?,?)',(name,age,department))
+    conn.commit()
+    row=row+1
+
+
+
+
+
+'------------------------------------'
 import xlsxwriter 
       
 workbook = xlsxwriter.Workbook('DBexcel.xlsx') 
@@ -33,9 +66,9 @@ column = 0
 data=conn.execute('select * from company_table').fetchall()
   
 for i in range(0,len(data)):
-    worksheet.write(row, column, str(data[i][1]))
-    worksheet.write(row, column+1, str(data[i][2]))
-    worksheet.write(row, column+2, str(data[i][3]))
+    worksheet.write(row, column, str(data[i][0]))
+    worksheet.write(row, column+1, str(data[i][1]))
+    worksheet.write(row, column+2, str(data[i][2]))
     row += 1
       
 workbook.close() 
